5118705c5cb7a2d5dd7253849d8c9dd87e254b95
seruva19 <seruva19@yandex.ru>
2026-03-03 01:25:50 +0300

    fix modules import crash when optional deps are missing

--- src/ayase/modules/__init__.py
+++ src/ayase/modules/__init__.py
@@ -4,239 +4,135 @@ All modules are auto-discovered at runtime by ``ModuleRegistry.discover_modules(
 This file exports the most commonly-used modules for direct import convenience::
 
     from ayase.modules import DOVERModule, VMAFModule, VideoScoreModule
+
+Modules whose optional dependencies are not installed are silently skipped.
 """
 
-# --- Core / basic ---
-from .basic import BasicQualityModule
-from .metadata import MetadataModule
-from .structural import StructuralModule
-from .exposure import ExposureModule
-from .compression_artifacts import CompressionArtifactsModule
+import importlib as _importlib
+import logging as _logging
 
-# --- Aesthetics ---
-from .aesthetic import AestheticModule
-from .aesthetic_scoring import AestheticScoringModule
-from .nima import NIMAModule
-from .laion_aesthetic import LAIONAestheticModule
+_logger = _logging.getLogger(__name__)
 
-# --- Text / OCR ---
-from .text import TextDetectionModule
-from .ocr_fidelity import OCRFidelityModule
-from .captioning import CaptioningModule
-
-# --- Motion & flow ---
-from .motion import MotionModule
-from .motion_smoothness import MotionSmoothnessModule
-from .motion_amplitude import MotionAmplitudeModule
-from .advanced_flow import AdvancedFlowModule
-from .camera_motion import CameraMotionModule
-from .kandinsky_motion import KandinskyMotionModule
-
-# --- Temporal consistency ---
-from .temporal_flickering import TemporalFlickeringModule
-from .clip_temporal import CLIPTemporalModule
-from .subject_consistency import SubjectConsistencyModule
-from .background_consistency import BackgroundConsistencyModule
-from .object_permanence import ObjectPermanenceModule
-from .color_consistency import ColorConsistencyModule
-from .temporal_style import TemporalStyleModule
-from .style_consistency import StyleConsistencyModule
-
-# --- Alignment ---
-from .semantic_alignment import SemanticAlignmentModule
-from .video_text_matching import VideoTextMatchingModule
-from .t2v_score import T2VScoreModule
-from .vqa_score import VQAScoreModule
-
-# --- No-reference quality ---
-from .dover import DOVERModule
-from .fast_vqa import FastVQAModule
-from .q_align import QAlignModule
-from .topiq import TOPIQModule
-from .musiq import MUSIQModule
-from .maniqa import MANIQAModule
-from .brisque import BRISQUEModule
-from .niqe import NIQEModule
-from .clip_iqa import CLIPIQAModule
-from .imaging_quality import ImagingQualityModule
-
-# --- Full-reference quality ---
-from .vmaf import VMAFModule
-from .dists import DISTSModule
-from .perceptual_fr import PerceptualFRModule
-
-# --- SOTA video quality (CVPR/NeurIPS/EMNLP 2024-2025) ---
-from .videoscore import VideoScoreModule
-from .video_reward import VideoRewardModule
-from .rqvqa import RQVQAModule
-from .aigv_assessor import AIGVAssessorModule
-from .finevq import FineVQModule
-from .kvq import KVQModule
-from .jedi_metric import JEDiModule
-from .cover import COVERModule
-from .videval import VIDEVALModule
-
-# --- Generation metrics ---
-from .fvd import FVDModule
-from .fvmd import FVMDModule
-from .inception_score import InceptionScoreModule
-from .i2v_similarity import I2VSimilarityModule
-from .sd_reference import SDReferenceModule
-
-# --- Face & human ---
-from .human_fidelity import HumanFidelityModule
-from .face_fidelity import FaceFidelityModule
-from .face_landmark_quality import FaceLandmarkQualityModule
-from .face_iqa import FaceIQAModule
-
-# --- Scene & content ---
-from .scene import SceneModule
-from .scene_detection import SceneDetectionModule
-from .scene_tagging import SceneTaggingModule
-from .object_detection import ObjectDetectionModule
-from .action_recognition import ActionRecognitionModule
-from .spatial_relationship import SpatialRelationshipModule
-from .physics import PhysicsModule
-from .commonsense import CommonsenseModule
-from .multiple_objects import MultipleObjectsModule
-
-# --- Safety & ethics ---
-from .nsfw import NSFWModule
-from .deepfake_detection import DeepfakeDetectionModule
-from .harmful_content import HarmfulContentModule
-from .watermark_classifier import WatermarkClassificationModule
-from .bias_detection import BiasDetectionModule
-
-# --- Audio ---
-from .audio import AudioModule
-from .audio_pesq import AudioPESQModule
-from .dnsmos import DNSMOSModule
-
-# --- HDR / codec ---
-from .hdr_metadata import HDRMetadataModule
-from .production_quality import ProductionQualityModule
-
-# --- Dataset operations ---
-from .dedup import DeduplicationModule
-from .embedding import EmbeddingModule
-from .diversity_selection import DiversitySelectionModule
-from .dataset_analytics import DatasetAnalyticsModule
-from .resolution_bucketing import ResolutionBucketingModule
-from .llm_advisor import LLMAdvisorModule
-
-# --- Utility ---
-from .cpbd import CPBDModule
-from .spectral import SpectralComplexityModule
-from .background_diversity import BackgroundDiversityModule
-from .video_memorability import VideoMemorabilityModule
-
-
-__all__ = [
-    # Core
-    "BasicQualityModule",
-    "MetadataModule",
-    "StructuralModule",
-    "ExposureModule",
-    "CompressionArtifactsModule",
-    # Aesthetics
-    "AestheticModule",
-    "AestheticScoringModule",
-    "NIMAModule",
-    "LAIONAestheticModule",
-    # Text / OCR
-    "TextDetectionModule",
-    "OCRFidelityModule",
-    "CaptioningModule",
-    # Motion
-    "MotionModule",
-    "MotionSmoothnessModule",
-    "MotionAmplitudeModule",
-    "AdvancedFlowModule",
-    "CameraMotionModule",
-    "KandinskyMotionModule",
-    # Temporal
-    "TemporalFlickeringModule",
-    "CLIPTemporalModule",
-    "SubjectConsistencyModule",
-    "BackgroundConsistencyModule",
-    "ObjectPermanenceModule",
-    "ColorConsistencyModule",
-    "TemporalStyleModule",
-    "StyleConsistencyModule",
-    # Alignment
-    "SemanticAlignmentModule",
-    "VideoTextMatchingModule",
-    "T2VScoreModule",
-    "VQAScoreModule",
-    # NR quality
-    "DOVERModule",
-    "FastVQAModule",
-    "QAlignModule",
-    "TOPIQModule",
-    "MUSIQModule",
-    "MANIQAModule",
-    "BRISQUEModule",
-    "NIQEModule",
-    "CLIPIQAModule",
-    "ImagingQualityModule",
-    # FR quality
-    "VMAFModule",
-    "DISTSModule",
-    "PerceptualFRModule",
-    # SOTA video quality
-    "VideoScoreModule",
-    "VideoRewardModule",
-    "RQVQAModule",
-    "AIGVAssessorModule",
-    "FineVQModule",
-    "KVQModule",
-    "JEDiModule",
-    "COVERModule",
-    "VIDEVALModule",
-    # Generation
-    "FVDModule",
-    "FVMDModule",
-    "InceptionScoreModule",
-    "I2VSimilarityModule",
-    "SDReferenceModule",
-    # Face & human
-    "HumanFidelityModule",
-    "FaceFidelityModule",
-    "FaceLandmarkQualityModule",
-    "FaceIQAModule",
-    # Scene & content
-    "SceneModule",
-    "SceneDetectionModule",
-    "SceneTaggingModule",
-    "ObjectDetectionModule",
-    "ActionRecognitionModule",
-    "SpatialRelationshipModule",
-    "PhysicsModule",
-    "CommonsenseModule",
-    "MultipleObjectsModule",
-    # Safety
-    "NSFWModule",
-    "DeepfakeDetectionModule",
-    "HarmfulContentModule",
-    "WatermarkClassificationModule",
-    "BiasDetectionModule",
-    # Audio
-    "AudioModule",
-    "AudioPESQModule",
-    "DNSMOSModule",
-    # HDR / codec
-    "HDRMetadataModule",
-    "ProductionQualityModule",
-    # Dataset ops
-    "DeduplicationModule",
-    "EmbeddingModule",
-    "DiversitySelectionModule",
-    "DatasetAnalyticsModule",
-    "ResolutionBucketingModule",
-    "LLMAdvisorModule",
-    # Utility
-    "CPBDModule",
-    "SpectralComplexityModule",
-    "BackgroundDiversityModule",
-    "VideoMemorabilityModule",
+# (attribute_name, module_path) pairs for lazy, fault-tolerant imports.
+_IMPORTS = [
+    # --- Core / basic ---
+    ("BasicQualityModule", ".basic"),
+    ("MetadataModule", ".metadata"),
+    ("StructuralModule", ".structural"),
+    ("ExposureModule", ".exposure"),
+    ("CompressionArtifactsModule", ".compression_artifacts"),
+    # --- Aesthetics ---
+    ("AestheticModule", ".aesthetic"),
+    ("AestheticScoringModule", ".aesthetic_scoring"),
+    ("NIMAModule", ".nima"),
+    ("LAIONAestheticModule", ".laion_aesthetic"),
+    # --- Text / OCR ---
+    ("TextDetectionModule", ".text"),
+    ("OCRFidelityModule", ".ocr_fidelity"),
+    ("CaptioningModule", ".captioning"),
+    # --- Motion & flow ---
+    ("MotionModule", ".motion"),
+    ("MotionSmoothnessModule", ".motion_smoothness"),
+    ("MotionAmplitudeModule", ".motion_amplitude"),
+    ("AdvancedFlowModule", ".advanced_flow"),
+    ("CameraMotionModule", ".camera_motion"),
+    ("KandinskyMotionModule", ".kandinsky_motion"),
+    # --- Temporal consistency ---
+    ("TemporalFlickeringModule", ".temporal_flickering"),
+    ("CLIPTemporalModule", ".clip_temporal"),
+    ("SubjectConsistencyModule", ".subject_consistency"),
+    ("BackgroundConsistencyModule", ".background_consistency"),
+    ("ObjectPermanenceModule", ".object_permanence"),
+    ("ColorConsistencyModule", ".color_consistency"),
+    ("TemporalStyleModule", ".temporal_style"),
+    ("StyleConsistencyModule", ".style_consistency"),
+    # --- Alignment ---
+    ("SemanticAlignmentModule", ".semantic_alignment"),
+    ("VideoTextMatchingModule", ".video_text_matching"),
+    ("T2VScoreModule", ".t2v_score"),
+    ("VQAScoreModule", ".vqa_score"),
+    # --- No-reference quality ---
+    ("DOVERModule", ".dover"),
+    ("FastVQAModule", ".fast_vqa"),
+    ("QAlignModule", ".q_align"),
+    ("TOPIQModule", ".topiq"),
+    ("MUSIQModule", ".musiq"),
+    ("MANIQAModule", ".maniqa"),
+    ("BRISQUEModule", ".brisque"),
+    ("NIQEModule", ".niqe"),
+    ("CLIPIQAModule", ".clip_iqa"),
+    ("ImagingQualityModule", ".imaging_quality"),
+    # --- Full-reference quality ---
+    ("VMAFModule", ".vmaf"),
+    ("DISTSModule", ".dists"),
+    ("PerceptualFRModule", ".perceptual_fr"),
+    # --- SOTA video quality (CVPR/NeurIPS/EMNLP 2024-2025) ---
+    ("VideoScoreModule", ".videoscore"),
+    ("VideoRewardModule", ".video_reward"),
+    ("RQVQAModule", ".rqvqa"),
+    ("AIGVAssessorModule", ".aigv_assessor"),
+    ("FineVQModule", ".finevq"),
+    ("KVQModule", ".kvq"),
+    ("JEDiModule", ".jedi_metric"),
+    ("COVERModule", ".cover"),
+    ("VIDEVALModule", ".videval"),
+    # --- Generation metrics ---
+    ("FVDModule", ".fvd"),
+    ("FVMDModule", ".fvmd"),
+    ("InceptionScoreModule", ".inception_score"),
+    ("I2VSimilarityModule", ".i2v_similarity"),
+    ("SDReferenceModule", ".sd_reference"),
+    # --- Face & human ---
+    ("HumanFidelityModule", ".human_fidelity"),
+    ("FaceFidelityModule", ".face_fidelity"),
+    ("FaceLandmarkQualityModule", ".face_landmark_quality"),
+    ("FaceIQAModule", ".face_iqa"),
+    # --- Scene & content ---
+    ("SceneModule", ".scene"),
+    ("SceneDetectionModule", ".scene_detection"),
+    ("SceneTaggingModule", ".scene_tagging"),
+    ("ObjectDetectionModule", ".object_detection"),
+    ("ActionRecognitionModule", ".action_recognition"),
+    ("SpatialRelationshipModule", ".spatial_relationship"),
+    ("PhysicsModule", ".physics"),
+    ("CommonsenseModule", ".commonsense"),
+    ("MultipleObjectsModule", ".multiple_objects"),
+    # --- Safety & ethics ---
+    ("NSFWModule", ".nsfw"),
+    ("DeepfakeDetectionModule", ".deepfake_detection"),
+    ("HarmfulContentModule", ".harmful_content"),
+    ("WatermarkClassificationModule", ".watermark_classifier"),
+    ("BiasDetectionModule", ".bias_detection"),
+    # --- Audio ---
+    ("AudioModule", ".audio"),
+    ("AudioPESQModule", ".audio_pesq"),
+    ("DNSMOSModule", ".dnsmos"),
+    # --- HDR / codec ---
+    ("HDRMetadataModule", ".hdr_metadata"),
+    ("ProductionQualityModule", ".production_quality"),
+    # --- Dataset operations ---
+    ("DeduplicationModule", ".dedup"),
+    ("EmbeddingModule", ".embedding"),
+    ("DiversitySelectionModule", ".diversity_selection"),
+    ("DatasetAnalyticsModule", ".dataset_analytics"),
+    ("ResolutionBucketingModule", ".resolution_bucketing"),
+    ("LLMAdvisorModule", ".llm_advisor"),
+    # --- Utility ---
+    ("CPBDModule", ".cpbd"),
+    ("SpectralComplexityModule", ".spectral"),
+    ("BackgroundDiversityModule", ".background_diversity"),
+    ("VideoMemorabilityModule", ".video_memorability"),
 ]
+
+# Perform imports, silently skipping modules with missing optional deps.
+_available = {}
+for _attr, _mod in _IMPORTS:
+    try:
+        _m = _importlib.import_module(_mod, __name__)
+        _obj = getattr(_m, _attr)
+        _available[_attr] = _obj
+        globals()[_attr] = _obj
+    except Exception:
+        pass
+
+__all__ = list(_available.keys())
