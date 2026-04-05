4fd44b41591f4635c2f1b60679cb4c997a9d4918
seruva19 <seruva19@yandex.ru>
2026-04-06 01:01:38 +0300

    bump: 0.1.27

--- src/ayase/__init__.py
+++ src/ayase/__init__.py
@@ -3,7 +3,7 @@ Ayase - Modular media quality metrics toolkit
 
 """
 
-__version__ = "0.1.26"
+__version__ = "0.1.27"
 __author__ = "Ayase Contributors"
 __description__ = "Modular media quality metrics toolkit"
 
